925db51f08d8a145c86384eac71691af76240bfc
Edwin <edwincl1234@gmail.com>
2024-08-29 20:56:44 -0500

    Detalles de productos

--- categorias/ProductosCategoria.php
+++ categorias/ProductosCategoria.php
@@ -7,8 +7,26 @@ use MyProject\ApiClient;
 $apiClient = new ApiClient();
 $dataProducts = $apiClient->getProductos();
 $dataCategorias = $apiClient->getCategorias();
-$dataProductsCategory = $apiClient->getProductosCategorias($_REQUEST['id']);
 
+// $parentCategory = 4;
+// $filteredCategories = array_filter($dataCategorias, function ($category) use ($parentCategory) {
+//     return $category['parent_category'] === $parentCategory;
+// });
+
+  if(isset($_REQUEST['id'])){
+    $dataProductsCategory = $apiClient->getProductosCategorias($_REQUEST['id']);
+    if(is_numeric($_REQUEST['id'])) {
+      $category_id = (int) $_REQUEST['id'];
+    }
+    $productsByCategory = [];
+    foreach ($dataProducts as $product) {
+        $productsByCategory[$product['category_id']][] = $product;
+    }
+    // Luego puedes acceder a los productos de una categoría específica directamente
+    $products = isset($productsByCategory[$category_id]) ? $productsByCategory[$category_id] : [];
+  }else{
+    $products = $dataProducts;
+  }
 
 include 'productoCategoria.html';
 ?>
---
+++ product_details.php
@@ -0,0 +1,21 @@
+<?php
+require_once 'ApiClient.php';
+require 'vendor/autoload.php';
+
+use MyProject\ApiClient;
+
+$apiClient = new ApiClient();
+$dataProducts = $apiClient->getProductos();
+$dataCategorias = $apiClient->getCategorias();
+
+
+$product_id = (int) $_REQUEST['id'];
+$productsById = [];
+foreach ($dataProducts as $product) {
+    $productsById[$product['id']][] = $product;
+}
+// Luego puedes acceder a los productos de una categoría específica directamente
+$products = isset($productsById[$product_id]) ? $productsById[$product_id] : [];
+
+include 'product_details.html';
+?>
